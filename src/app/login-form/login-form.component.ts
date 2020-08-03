adc35d9fbdcee775385ca92fe98c2fe48e373855
NicholasCF <nicholas.fernando@u.nus.edu>
2020-08-03 11:39:48 +0800

    Remove debug statements

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -24,7 +24,6 @@ export class LoginFormComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router
   ) {
-    console.log(loginService.currentUserValue);
   }
 
   ngOnInit(): void {
